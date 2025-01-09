921eaa06ae6cf3a5936eeef626132d2d20f77c7c
gershon horowitz <gershon.horowitz@gmail.com>
2025-01-09 10:20:00 +0200

    Updates

---
+++ precache-manifest.6d2d99badbcd596b6988768cfb937b31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff83c1e926b51b0f3b1066e3bcb8595b",
+    "url": "/pictext/index.html"
+  },
+  {
+    "revision": "640ecd9b905abcbc3b58",
+    "url": "/pictext/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1f2f036adc8834813874",
+    "url": "/pictext/static/js/2.7e6452d0.chunk.js"
+  },
+  {
+    "revision": "d5a2140746d4a4e5c89e",
+    "url": "/pictext/static/js/3.e13311ee.chunk.js"
+  },
+  {
+    "revision": "640ecd9b905abcbc3b58",
+    "url": "/pictext/static/js/main.b256e350.chunk.js"
+  },
+  {
+    "revision": "0e512ac653514cd0d843",
+    "url": "/pictext/static/js/runtime~main.d1e36f6b.js"
+  }
+]);
